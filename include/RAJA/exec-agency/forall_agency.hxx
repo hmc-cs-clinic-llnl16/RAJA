444398cf456abbd94b8739b9ea7a4373bf50fe04
Dan Obermiller <dobermiller16@cmc.edu>
2016-10-12 14:57:42 -0700

    Add initial Agency implementation
    - Write tests to use Agency's Forall implementation
    - Add Agency policies for Agency default, as well as OMP & CUDA
    - Add Agency OMP and CUDA policies
    - Start implementation of forallN for agency
    - Add more useless folders and files to .gitignore

---
+++ include/RAJA/exec-agency/forall_agency.hxx
@@ -0,0 +1,155 @@
+/*!
+ ******************************************************************************
+ *
+ * \file
+ *
+ * \brief   Header file containing RAJA index set and segment iteration
+ *          template methods for Agency.
+ *
+ *          These methods should work on any platform that supports Agency.
+ *
+ ******************************************************************************
+ */
+
+#ifndef RAJA_forall_agency_HXX
+#define RAJA_forall_agency_HXX
+
+#include "RAJA/config.hxx"
+
+#if defined(RAJA_ENABLE_AGENCY)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+//
+// Produced at the Lawrence Livermore National Laboratory
+//
+// LLNL-CODE-689114
+//
+// All rights reserved.
+//
+// This file is part of RAJA.
+//
+// For additional details, please also read RAJA/LICENSE.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the disclaimer below.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the disclaimer (as noted below) in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of the LLNS/LLNL nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+// LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+#include "RAJA/int_datatypes.hxx"
+#include "RAJA/fault_tolerance.hxx"
+#include "RAJA/segment_exec.hxx"
+
+#include "agency/agency.hpp"
+
+#include <thread>
+#include <iterator>
+#include <algorithm>
+
+namespace RAJA
+{
+
+namespace experimental
+{
+
+/// ASSUMPTIONS:
+///   1- That we can get the number of threads this way
+///   2- That the number of workers can be sensibly computed this way
+///   3- That the number of workers computed this way works for 
+///      sequenctuial execution.
+template <typename Func, typename Agent, typename Worker>
+RAJA_INLINE void forall(const agency_base<Agent, Worker>&, 
+                        const RangeSegment& iter, 
+                        Func&& loop_body)
+{
+  auto numThreads = std::max(std::thread::hardware_concurrency(), 1u);
+  auto workPerThread = (std::end(iter) - std::begin(iter)) / numThreads;
+
+  agency::bulk_invoke(Worker{}(numThreads),
+                      [=](Agent& self) {
+                        auto start = workPerThread * self.index();
+                        auto end = self.index() == (numThreads - 1) 
+                                     ? iter.getEnd()
+                                     : start + workPerThread;
+                        for (auto i = start; i < end; ++i) {
+                          loop_body(i);  
+                        }
+                      });
+}
+
+template <typename Iterable, typename Func, typename Agent, typename Worker>
+RAJA_INLINE void forall(const agency_base<Agent, Worker>&,
+                        Iterable&& iter,
+                        Func&& loop_body)
+{
+  auto begin = std::begin(iter);
+
+  auto numThreads = std::max(std::thread::hardware_concurrency(), 1u);
+  auto workPerThread = (std::end(iter) - begin) / numThreads;
+
+  agency::bulk_invoke(Worker{}(numThreads),
+                      [=](Agent& self) {
+                        auto start = workPerThread * self.index();
+                        auto end = self.index() == (numThreads - 1) 
+                                     ? iter.getEnd()
+                                     : start + workPerThread;
+                        for (auto i = start; i < end; ++i) {
+                          loop_body(begin[i]);  
+                        }
+                      });
+}
+
+template <typename Iterable, typename Func, typename Agent, typename Worker>
+RAJA_INLINE void forall_Icount(const agency_base<Agent, Worker>&,
+                               Iterable&& iter,
+                               Index_type icount,
+                               Func&& loop_body)
+{
+  auto begin = std::begin(iter);
+
+  auto numThreads = std::max(std::thread::hardware_concurrency(), 1u);
+  auto workPerThread = std::distance(begin, std::end(iter)) / numThreads;
+
+  agency::bulk_invoke(Worker{}(numThreads),
+                      [=](Agent& self) {
+                        auto start = workPerThread * self.index();
+                        auto end = self.index() == (numThreads - 1) 
+                                     ? iter.getEnd()
+                                     : start + workPerThread;
+                        for (auto i = start; i < end; ++i) {
+                          loop_body(i + icount, begin[i]);  
+                        }
+                      });
+}
+
+} // closing brace for experimental namespace
+
+}  // closing brace for RAJA namespace
+
+#endif  // closing endif for if defined(RAJA_ENABLE_AGENCY)
+
+#endif  // closing endif for header file include guard
---
+++ test/unit/cpu/test-agency.cxx
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+ *
+ * Produced at the Lawrence Livermore National Laboratory.
+ *
+ * All rights reserved.
+ *
+ * For release details and restrictions, please see RAJA/LICENSE.
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "RAJA/RAJA.hxx"
+#include "gtest/gtest.h"
+
+class AgencyTest : public ::testing::Test
+{
+protected:
+  virtual void SetUp()
+  {
+    expected = std::vector<double>(1000);
+    actual = std::vector<double>(1000);
+
+    y = std::vector<double>(1000);
+    x = std::vector<double>(1000);
+
+    for (auto i = 0; i < 1000; ++i) {
+        y[i] = i;
+        x[i] = 1000 - i;
+    }
+  }
+
+  std::vector<double> expected;
+  std::vector<double> actual;
+
+  std::vector<double> y;
+  std::vector<double> x;
+
+  template <typename Policy>
+  void forall_daxpy() {
+      double a = 17.3;
+
+      // Do expected result
+      for (auto i = 0; i < 1000; ++i) {
+        expected[i] = a*x[i] + y[i];
+      }
+
+      // Do actual result
+      RAJA::forall<Policy>(
+        0, 1000, [=](RAJA::Index_type i) {
+          actual[i] = a*x[i] + y[i];
+        });
+
+      // Validate_result
+      for (auto i = 0; i < 1000; ++i) {
+        EXPECT_EQ(actual[i], expected[i]);
+      }
+  }
+};
+
+TEST_F(AgencyTest, forall_daxpy_parallel)
+{
+    forall_daxpy<RAJA::experimental::agency_parallel_exec>();
+}
+
+TEST_F(AgencyTest, forall_daxpy_sequential)
+{
+    forall_daxpy<RAJA::experimental::agency_sequential_exec>();
+}
+
+#if 0
+#if defined(RAJA_ENABLE_OPENMP)
+TEST_F(AgencyTest, forall_daxpy_omp_parallel)
+{
+    forall_daxpy<RAJA::experimental::agency_omp_parallel_exec>();
+}
+
+TEST_F(AgencyTest, forall_daxpy_omp_sequential)
+{
+    forall_daxpy<RAJA::experimental::agency_omp_sequential_exec>();
+}
+#endif // defined(RAJA_ENABLE_OPENMP)
+
+// TODO: Write icount tests
+// TODO: Make sure we test all of: RangeSegment, Iterable, Container
+
+#if defined(RAJA_ENABLE_NESTED)
+
+TEST_F(AgencyTest, forallN_mmult_parallel)
+{
+
+}
+
+TEST_F(AgencyTest, forallN_mmult_sequential)
+{
+
+}
+
+#if defined(RAJA_ENABLE_OPENMP)
+TEST_F(AgencyTest, forallN_mmult_omp_parallel)
+{
+
+}
+
+TEST_F(AgencyTest, forallN_mmult_omp_sequential)
+{
+
+}
+#endif // defined(RAJA_ENABLE_OPENMP)
+#endif
+#endif // defined RAJA_ENABLE_NESTED
